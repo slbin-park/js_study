8e43419088725149f77d20bfa737ae2ab22a1cdf
slbin-park <smpts00@naver.com>
2022-02-14 15:15:58 +0900

    Add 문자열 js study

---
+++ 자료구조와 자료형/3.js
@@ -0,0 +1,21 @@
+// 문자열
+// '' == "" 두개 같다.
+let lst = `손님:
+* Jon
+* Pet
+* Mar
+`
+console.log(lst)
+
+// 문자열의 불변성
+// 문자열을 수정이 불가
+// 즉 중간 글자를 바꿀려면 에러가 난다.
+// toUpperCase()
+// toLowerCase()
+// 두가지 메소드가 있음
+// lastIndexOf(str)
+// includes == 포함
+// startsWith == 시작문자
+// endsWith 마지막 문자열
+// slice(0,5) 0~5번째 위치까지 0은 포함 , 5번째 위치는 포함하지않음
+
